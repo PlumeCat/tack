ef400b49c90cba25999fd0d0390ab60c59736159
James <jamesmjdm@gmail.com>
2016-11-10 13:08:54 +0000

    ast: improved ast construction system and added several node parsers

--- ast.h
+++ ast.h
@@ -1,25 +1,6 @@
 #ifndef _AST_H
 #define _AST_H
 
-typedef vector<Symbol>::iterator SymbolIter;
-
-enum AstNodeType
-{
-	AST_SCRIPT = 1,
-
-	AST_FUNC_DECL,
-	AST_VAR_STATEMENT,
-	AST_IMPORT_STATEMENT,
-
-	AST_IDENTIFIER,
-	AST_EXPRESSION,
-
-	AST_CONST_STRING,
-	AST_CONST_NUMBER,
-};
-
-
-
 /*
 AST Node definitions:
 
@@ -27,54 +8,52 @@ AST Node definitions:
 	// x | y 	x or y
 	// [ x ]	x is optional
 
-	Script
+	√ Script
 		{ ImportStatement | FuncDecl | VarStatement }
 
-	FuncDecl
-		Identifier, { ArgDef }, StatementBlock
+	√ FuncDecl
+		Identifier, ArgsDecl, StatementBlock
 
-	ArgDef
+	√ ArgsDecl
+		[ { Identifier } ]
+
+	√ VarStatement
 		Identifier, [ Expression ]
 
-	VarStatement
-		Identifier, [ Expression ]
-
-	ImportStatement
+	√ ImportStatement
 		Identifier
 
-	IfConstruct
-		IfBlock, [ { ElseIfBlock } ], [ ElseBlock ]
+	√ IfConstruct
+		IfBlock, [ ElseBlock ]
 
-	IfBlock
+	√ IfBlock
 		Expression, StatementBlock
 
-	ElseIfBlock
+	√ ElseBlock
+		StatementBlock, IfConstruct
+
+
+	√ WhileLoop
 		Expression, StatementBlock
 
-	ElseBlock
-		StatementBlock
-
-	WhileLoop
-		Expression, StatementBlock
-
-	StatementBlock
+	√ StatementBlock
 		{ 	
-			IfConstruct | 
-			WhileLoop | 
-			BreakStatement | 
-			ContinueStatement | 
-			ReturnStatement | 
+			√ IfConstruct | 
+			√ WhileLoop | 
+			√ BreakStatement | 
+			√ ContinueStatement | 
+			√ ReturnStatement | 
 			VarStatement | 
 			ExpressionStatement 
 		}
 
-	BreakStatement
+	√ BreakStatement
 		[]
 
-	ContinueStatement
+	√ ContinueStatement
 		[]
 
-	ReturnStatement
+	√ ReturnStatement
 		[ Expression ]
 
 	ExpressionStatement
@@ -105,11 +84,49 @@ AST Node definitions:
 		
 */
 
+typedef vector<Symbol>::iterator SymbolIter;
+
+enum AstNodeType
+{
+	AST_SCRIPT = 1,
+
+	AST_FUNC_DECL,
+	AST_VAR_STATEMENT,
+	AST_IMPORT_STATEMENT,
+	AST_ARGS_DECL,
+	AST_STATEMENT_BLOCK,
+	AST_IF_CONSTRUCT,
+	AST_IF_BLOCK,
+	AST_ELSE_BLOCK,
+	AST_WHILE_CONSTRUCT,
+
+	AST_RETURN_STATEMENT,
+	AST_CONTINUE_STATEMENT,
+	AST_BREAK_STATEMENT,
+
+	AST_IDENTIFIER,
+	AST_CONST_STRING,
+	AST_CONST_NUMBER,
+	AST_EXPRESSION,
+};
+
+
 map<AstNodeType, string> astNodeNames = {
 	{ AST_SCRIPT, 			"Script" },
 	{ AST_FUNC_DECL, 		"FuncDecl" },
 	{ AST_VAR_STATEMENT,	"VarStatement" },
 	{ AST_IMPORT_STATEMENT, "ImportStatement" },
+
+	{ AST_ARGS_DECL, 		"ArgsDecl" },
+	{ AST_STATEMENT_BLOCK,	"StatementBlock" },
+	{ AST_IF_CONSTRUCT ,	"IfConstruct"	},
+	{ AST_IF_BLOCK,			"IfBlock" },
+	{ AST_ELSE_BLOCK,		"ElseBlock" },
+	{ AST_WHILE_CONSTRUCT,	"WhileConstruct" },
+
+	{ AST_RETURN_STATEMENT,	"ReturnStatement" },
+	{ AST_CONTINUE_STATEMENT,"ContinueStatement" },
+	{ AST_BREAK_STATEMENT,	"BreakStatement" },
 
 	{ AST_IDENTIFIER,		"Identifier" },
 	{ AST_CONST_STRING,		"StringConstant" },
@@ -148,47 +165,61 @@ struct AstNode
 	}
 };
 
-AstNode* parseVarStatement(SymbolIter& symbol)
+
+typedef AstNode* (*AstNodeFunc)(SymbolIter& iter);
+bool expect(SymbolIter& iter, AstNodeFunc func, AstNode* parent)
 {
-	SymbolIter orig = symbol;
-	AstNode* node = new AstNode(AST_VAR_STATEMENT);
-
-	if (symbol->type == SYMBOL_VAR)
+	SymbolIter orig = iter;
+	AstNode* child = func(iter);
+	if (child)
 	{
-		symbol++;
-	}
-	else
-	{
-		goto fail;
+		parent->children.push_back(child);
+		return true;
 	}
 
-	if (symbol->type == SYMBOL_NAME)
-	{
-		AstNode* name = new AstNode(AST_IDENTIFIER, symbol->data);
-		node->children.push_back(name);
-		symbol++;
-	}
-	else
-	{
-		goto fail;
-	}
-
-	if (symbol->type == SYMBOL_SEMICOLON)
-	{
-		symbol++;
-	}
-	else
-	{
-		goto fail;
-	}
-
-	return node;
-
-fail:
-	delete node;
-	symbol = orig;
-	return nullptr;
+	iter = orig;
+	return false;
 }
+bool expect(SymbolIter& iter, SymbolType type)
+{
+	if (iter->type == type)
+	{
+		iter++;
+		return true;
+	}
+	return false;
+}
+bool expect(SymbolIter& iter, SymbolType type, string& outData)
+{
+	if (iter->type == type)
+	{
+		outData = iter->data;
+		iter++;
+		return true;
+	}
+
+	return false;
+}
+
+AstNode* parseImportStatement(SymbolIter&);
+AstNode* parseFuncDecl(SymbolIter&);
+AstNode* parseArgsDecl(SymbolIter&);
+AstNode* parseVarStatement(SymbolIter&);
+AstNode* parseIfConstruct(SymbolIter&);
+AstNode* parseIfBlock(SymbolIter&);
+AstNode* parseElseBlock(SymbolIter&);
+AstNode* parseWhileLoop(SymbolIter&);
+AstNode* parseStatementBlock(SymbolIter&);
+AstNode* parseBreakStatement(SymbolIter&);
+AstNode* parseContinueStatement(SymbolIter&);
+AstNode* parseReturnStatement(SymbolIter&);
+AstNode* parseExpressionStatement(SymbolIter&);
+AstNode* parseExpression(SymbolIter&);
+AstNode* parseSubExpression(SymbolIter&);
+AstNode* parseAssignExpression(SymbolIter&);
+AstNode* parseTernaryExpression(SymbolIter&);
+AstNode* parseBinaryExpression(SymbolIter&);
+AstNode* parseUnaryExpression(SymbolIter&);
 
 AstNode* parseExpression(SymbolIter& symbol)
 {
@@ -220,65 +251,15 @@ fail:
 	return nullptr;	
 }
 
-AstNode* parseVarStatement2(SymbolIter& symbol)
+
+AstNode* parseIdentifier(SymbolIter& iter)
 {
-	SymbolIter orig = symbol;
-	AstNode* node = new AstNode(AST_VAR_STATEMENT);
-	AstNode* child = nullptr;
-
-	if (symbol->type == SYMBOL_VAR)
+	string data;
+	if (expect(iter, SYMBOL_NAME, data))
 	{
-		symbol++;
+		AstNode* node = new AstNode(AST_IDENTIFIER, data);
+		return node;
 	}
-	else
-	{
-		goto fail;
-	}
-
-	if (symbol->type == SYMBOL_NAME)
-	{
-		AstNode* name = new AstNode(AST_IDENTIFIER, symbol->data);
-		node->children.push_back(name);
-		symbol++;
-	}
-	else
-	{
-		goto fail;
-	}
-
-	if (symbol->type == SYMBOL_OP_ASSIGN)
-	{
-		symbol++;
-	}
-	else
-	{
-		goto fail;
-	}
-
-	child = parseExpression(symbol);
-	if (child)
-	{
-		node->children.push_back(child);
-	}
-	else
-	{
-		goto fail;
-	}
-
-	if (symbol->type == SYMBOL_SEMICOLON)
-	{
-		symbol++;
-	}
-	else
-	{
-		goto fail;
-	}
-
-	return node;
-
-fail:
-	delete node;
-	symbol = orig;
 	return nullptr;
 }
 
@@ -287,44 +268,217 @@ AstNode* parseImportStatement(SymbolIter& symbol)
 	SymbolIter orig = symbol;
 	AstNode* node = new AstNode(AST_IMPORT_STATEMENT);
 
-	if (symbol->type == SYMBOL_IMPORT)
-	{
-		symbol++;
-	}
-	else
-	{
-		goto fail;
-	}
+	if (expect(symbol, SYMBOL_IMPORT))
+	if (expect(symbol, parseIdentifier, node))
+	if (expect(symbol, SYMBOL_SEMICOLON))
+		return node;
 
-	if (symbol->type == SYMBOL_NAME)
-	{
-		AstNode* name = new AstNode(AST_IDENTIFIER, symbol->data);
-		node->children.push_back(name);
-		symbol++;
-	}
-	else
-	{
-		goto fail;
-	}
-
-	if (symbol->type == SYMBOL_SEMICOLON)
-	{
-		symbol++;
-	}
-	else
-	{
-		goto fail;
-	}
-
-	return node;
-
-fail:
 	delete node;
 	symbol = orig;
 	return nullptr;
 }
 
 
+AstNode* parseVarStatement(SymbolIter& symbol)
+{
+	SymbolIter orig = symbol;
+	AstNode* node = new AstNode(AST_VAR_STATEMENT);
+
+	if (expect(symbol, SYMBOL_VAR))
+	if (expect(symbol, parseIdentifier, node))
+	{
+		if (expect(symbol, SYMBOL_SEMICOLON))
+			return node;
+		else 
+		{
+			if (expect(symbol, SYMBOL_OP_ASSIGN))
+			if (expect(symbol, parseExpression, node))
+			if (expect(symbol, SYMBOL_SEMICOLON))
+				return node;
+		}
+	}
+
+	delete node;
+	symbol = orig;
+	return nullptr;
+}
+AstNode* parseArgsDecl(SymbolIter& symbol)
+{
+	SymbolIter orig = symbol;
+	AstNode* node = new AstNode(AST_ARGS_DECL);
+
+	if (expect(symbol, SYMBOL_LPAREN))
+	while (expect(symbol, parseIdentifier, node) && 
+		   expect(symbol, SYMBOL_COMMA)) {}
+	if (expect(symbol, SYMBOL_RPAREN))
+		return node;
+
+	delete node;
+	symbol = orig;
+	return nullptr;
+}
+
+AstNode* parseIfBlock(SymbolIter& symbol)
+{
+	SymbolIter orig = symbol;
+	AstNode* node = new AstNode(AST_IF_BLOCK);
+
+	if (expect(symbol, SYMBOL_IF))
+	if (expect(symbol, SYMBOL_LPAREN))
+	if (expect(symbol, parseExpression, node))
+	if (expect(symbol, SYMBOL_RPAREN))
+	if (expect(symbol, parseStatementBlock, node))
+		return node;
+
+	symbol = orig;
+	delete node;
+	return nullptr;
+}
+AstNode* parseElseBlock(SymbolIter& symbol)
+{
+	SymbolIter& orig = symbol;
+	AstNode* node = new AstNode(AST_ELSE_BLOCK);
+
+	if (expect(symbol, SYMBOL_ELSE))
+	if (expect(symbol, parseStatementBlock, node) ||
+		expect(symbol, parseIfConstruct, node))
+		return node;
+
+	symbol = orig;
+	delete node;
+	return nullptr;
+}
+AstNode* parseIfConstruct(SymbolIter& symbol)
+{
+	SymbolIter orig = symbol;
+	AstNode* node = new AstNode(AST_IF_CONSTRUCT);
+
+	if (expect(symbol, parseIfBlock, node))
+	if (expect(symbol, parseElseBlock, node) || true)
+		return node;
+
+	symbol = orig;
+	delete node;
+	return nullptr;
+}
+AstNode* parseWhileLoop(SymbolIter& symbol)
+{
+	SymbolIter orig = symbol;
+	AstNode* node = new AstNode(AST_WHILE_CONSTRUCT);
+
+	if (expect(symbol, SYMBOL_WHILE))
+	if (expect(symbol, SYMBOL_LPAREN))
+	if (expect(symbol, parseExpression, node))
+	if (expect(symbol, SYMBOL_RPAREN))
+	if (expect(symbol, parseStatementBlock, node))
+		return node;
+
+	symbol = orig;
+	delete node;
+	return nullptr;
+}
+AstNode* parseSubExpression(SymbolIter& symbol)
+{
+	SymbolIter orig = symbol;
+	AstNode* node = new AstNode(AST_SUB_EXPRESSION);
+
+	if (expect(symbol, SYMBOL_LPAREN))
+	if (expect(symbol, parseExpression, node))
+	if (expect(symbol, SYMBOL_RPAREN))
+		return node;
+
+	delete node;
+	symbol = orig;
+	return nullptr;
+}
+AstNode* parseStatementBlock(SymbolIter& symbol)
+{
+	SymbolIter orig = symbol;
+	AstNode* node = new AstNode(AST_STATEMENT_BLOCK);
+
+	if (expect(symbol, SYMBOL_LBRACE))
+	{
+		while (expect(symbol, parseVarStatement, node) ||
+			   expect(symbol, parseIfConstruct, node) ||
+			   expect(symbol, parseWhileLoop, node) || 
+			   expect(symbol, parseReturnStatement, node) ||
+			   expect(symbol, parseBreakStatement, node) ||
+			   expect(symbol, parseContinueStatement, node)) {}
+		if (expect(symbol, SYMBOL_RBRACE))
+			return node;
+	}
+
+	delete node;
+	symbol = orig;
+	return nullptr;
+}
+AstNode* parseReturnStatement(SymbolIter& symbol)
+{
+	SymbolIter orig = symbol;
+	AstNode* node = new AstNode(AST_RETURN_STATEMENT);
+
+	if (expect(symbol, SYMBOL_RETURN))
+	if (expect(symbol, parseExpression, node))
+	{
+		if (expect(symbol, SYMBOL_SEMICOLON))
+		{
+			return node;
+		}
+	}
+	else
+	{
+		if (expect(symbol, SYMBOL_SEMICOLON))
+		{
+			return node;
+		}
+	}
+
+
+	delete node;
+	symbol = orig;
+	return nullptr;
+}
+AstNode* parseBreakStatement(SymbolIter& symbol)
+{
+	SymbolIter orig = symbol;
+	AstNode* node = new AstNode(AST_BREAK_STATEMENT);
+
+	if (expect(symbol, SYMBOL_BREAK))
+	if (expect(symbol, SYMBOL_SEMICOLON))
+		return node;
+
+	delete node;
+	symbol = orig;
+	return nullptr;
+}
+AstNode* parseContinueStatement(SymbolIter& symbol)
+{
+	SymbolIter orig = symbol;
+	AstNode* node = new AstNode(AST_CONTINUE_STATEMENT);
+
+	if (expect(symbol, SYMBOL_CONTINUE))
+	if (expect(symbol, SYMBOL_SEMICOLON))
+		return node;
+
+	delete node;
+	symbol = orig;
+	return nullptr;
+}
+AstNode* parseFuncDecl(SymbolIter& symbol)
+{
+	SymbolIter orig = symbol;
+	AstNode* node = new AstNode(AST_FUNC_DECL);
+
+	if (expect(symbol, SYMBOL_FUNCTION))
+	if (expect(symbol, parseIdentifier, node))
+	if (expect(symbol, parseArgsDecl, node))
+	if (expect(symbol, parseStatementBlock, node))
+		return node;
+
+	delete node;
+	symbol = orig;
+	return nullptr;
+}
 
 AstNode* parseScript(vector<Symbol>& symbols)
 {
@@ -333,36 +487,17 @@ AstNode* parseScript(vector<Symbol>& symbols)
 	AstNode* node = new AstNode(AST_SCRIPT);
 	while (symbol != symbols.end())
 	{
-		AstNode* child = parseImportStatement(symbol);
-		if (child)
+		if (expect(symbol, parseImportStatement, node) ||
+			expect(symbol, parseVarStatement, node) ||
+			expect(symbol, parseFuncDecl, node))
 		{
-			node->children.push_back(child);
-			continue;
+			continue;	
 		}
-
-		child = parseVarStatement(symbol);
-		if (child)
+		else
 		{
-			node->children.push_back(child);
-			continue;
+			delete node;
+			return nullptr;
 		}
-
-		child = parseVarStatement2(symbol);
-		if (child)
-		{
-			node->children.push_back(child);
-			continue;
-		}
-
-		// child = parseFuncDecl(symbol);
-		// if (child)
-		// {
-		// 	node->children.push_back(child);
-		// 	continue;
-		// }
-
-		delete node;
-		return nullptr;
 	}
 
 	return node;
